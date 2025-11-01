8963da5708bd21c6405102f2b125136e2cd805b8
Umme-25 <ummehabiba2241@gmail.com>
2025-11-01 15:50:45 +0530

    Add files via upload

---
+++ asgn_1.py
@@ -0,0 +1,18 @@
+#Task 1
+#Taking input from user
+num1 = int(input("Enter first number:"))
+num2 = int(input("Enter second number:"))
+#operations and results displaying
+print(f"Addition: {num1 + num2}")
+print(f"Subtraction: {num1 - num2}")
+print(f"Multiplication: {num1 * num2}")
+print(f"Division: {num1 / num2}")
+
+#Task 2
+#Taking input of name from user.
+first_name=input("Enter your first name:")
+last_name=input("enter your last name")
+#Concatinating to full name
+full_name = first_name + " " + last_name
+#Displaying the Greetings
+print(f"Hello, {full_name}! Welcome to the Python program.")
